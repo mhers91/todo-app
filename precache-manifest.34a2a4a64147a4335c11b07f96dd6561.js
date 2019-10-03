fec80a37a14ac5f6cda8534f4808c76d1047e862
mhers91 <mhers91@gmail.com>
2019-10-03 13:31:07 -0400

    Updates

---
+++ precache-manifest.34a2a4a64147a4335c11b07f96dd6561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71950dbdf3d01ad123a5ddba4406e743",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "9bdc0e23d375c86428c6",
+    "url": "/todo-app/static/css/2.0af631bd.chunk.css"
+  },
+  {
+    "revision": "a2a9489f104fc8165058",
+    "url": "/todo-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9bdc0e23d375c86428c6",
+    "url": "/todo-app/static/js/2.5d699260.chunk.js"
+  },
+  {
+    "revision": "a2a9489f104fc8165058",
+    "url": "/todo-app/static/js/main.0650a909.chunk.js"
+  },
+  {
+    "revision": "2d385898d1bb583e95d3",
+    "url": "/todo-app/static/js/runtime-main.8a5791d9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todo-app/static/media/logo.25bf045c.svg"
+  }
+]);
